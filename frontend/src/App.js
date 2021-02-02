ec7ed98322653ad6a5f55d2f1a46177f4574eaaf
Tracy Falba <tfalba@mac.com>
2021-02-02 09:05:43 -0500

    nav dropdown sends to two other list pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,21 +18,24 @@ function App () {
 
   // Make this work like a filter to select which card list to present... can be a render of all Cards if...
   function showCards (filter) {
-    setCards(filter) // instead of a boolean make this a choice
+    if (token) {
+      setCards(filter)
+    }
+    // instead of a boolean make this a choice
   }
 
   return (
     <>
       <Navbar bg='light' expand='lg'>
-        <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href='#home'>Social Circle Cards</Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='mr-auto'>
             <Nav.Link href='#home'>Home</Nav.Link>
             <Nav.Link href='#link'>Link</Nav.Link>
             <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
-              <NavDropdown.Item onClick={() => showCards(1)} href='#action/3.1'>My Cards</NavDropdown.Item>
-              <NavDropdown.Item href='#action/3.2'>Another action</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => showCards(1)}>My Cards</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => showCards(2)}>Friends Cards</NavDropdown.Item>
               <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href='#action/3.4'>Separated link</NavDropdown.Item>
@@ -47,7 +50,9 @@ function App () {
             : <LoginComponent setAuth={setAuth} />}
         </Navbar.Collapse>
       </Navbar>
+      {((listCards === 0) && token && (<h3>All the Cards</h3>))}
       {((listCards === 1) && token) && (<h3>Here is my Cards</h3>)}
+      {((listCards === 2) && token) && (<h3>Here are friends Cards</h3>)}
     </>
   )
 }
