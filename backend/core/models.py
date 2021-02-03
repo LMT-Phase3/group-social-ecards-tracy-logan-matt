b8b7bffbb93fc5995d8a5d1fc992dc588e159845
Logan Robinson <loganrobinson315@gmail.com>
2021-02-03 10:40:43 -0500

    models made with card API endpoint

--- backend/core/models.py
+++ backend/core/models.py
@@ -2,4 +2,21 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    friends = models.ManyToManyField('User', symmetrical=False, related_name='followers')
     pass
+    def __str__(self):
+        return self.username
+
+class Card(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    background = models.CharField(max_length=100, null=True)
+    font = models.CharField(max_length=100, null=True)
+    border = models.CharField(max_length=100, null=True)
+    title = models.CharField(max_length=100, null=False)
+    image_front = models.CharField(max_length=500, null=True)
+    image_back = models.CharField(max_length=500, null=True)
+    message = models.TextField(max_length=5000, blank=False)
+
+
+    def __str__(self):
+        return self.title
--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Card
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,4 +7,21 @@ class UserSerializer(serializers.ModelSerializer):
         fields = [
             'pk',
             'username',
+        ]
+
+
+class CardSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(read_only=True, slug_field='username')
+    class Meta:
+        model = Card
+        fields = [
+            'user',
+            'pk',
+            'background',
+            'font',
+            'border',
+            'title',
+            'image_front',
+            'image_back',
+            'message',
         ]
