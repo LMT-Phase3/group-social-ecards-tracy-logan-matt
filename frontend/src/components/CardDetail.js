870cf651bc0f44d8b437e60d97ff1f1f0e4ea312
Tracy Falba <tfalba@mac.com>
2021-02-03 17:40:37 -0500

    some early formatting for card view

--- frontend/src/components/CardDetail.js
+++ frontend/src/components/CardDetail.js
@@ -14,11 +14,14 @@ const CardDetail = ({ token, pk }) => {
   return (
     <>
       {card && (
-        <div className='ml-sm-4 mr-sm-4'>
+        <div className='ml-sm-4 mr-sm-4 card-container'>
 
-          <div>Title: {card.title}</div>
-          <div>User: {card.user}</div>
-          <div>Message: {card.message}</div>
+          <div className='myfont-big'>{card.title}</div>
+          <div className='flex'>
+            <div style={{ border: '10px solid yellowgreen' }} className='myimage' />
+            <div style={{ justifyContent: 'center', backgroundColor: '#12125f', border: '10px solid yellowgreen' }} className='flex mybackground animate__animated animate__fadeInRight'><span className='myfont-small'>{card.message}</span></div>
+          </div>
+          {/* <div>User: {card.user}</div> */}
 
         </div>
       )}
