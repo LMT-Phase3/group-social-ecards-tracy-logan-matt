4746a81ba2e9db1219c2a93d4f2f129758804211
Logan Robinson <loganrobinson315@gmail.com>
2021-02-03 10:52:34 -0500

    added null to user field

--- backend/core/models.py
+++ backend/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    friends = models.ManyToManyField('User', symmetrical=False, related_name='followers')
+    friends = models.ManyToManyField('User', symmetrical=False, related_name='followers', null=True, blank=True)
     pass
     def __str__(self):
         return self.username
