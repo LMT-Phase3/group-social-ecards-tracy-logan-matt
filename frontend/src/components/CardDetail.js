08aec1c94813cdbd0de8b0323febdf32e01ff731
Matthew Smith <matthewbenton224@gmail.com>
2021-02-04 16:29:15 -0500

    lots of changes-fonts, styling, etc etc etc

--- frontend/src/components/CardDetail.js
+++ frontend/src/components/CardDetail.js
@@ -34,10 +34,10 @@ const CardDetail = ({ idx, token, pk, setViewDetail, viewDetail, cards }) => {
     <>
       {card && (
         <>
-          <div className='flex-col' style={{ alignItems: 'center', marginTop: '30px' }}>
-            <Link className='ml-sm-4' style={{ fontSize: '20px' }} onClick={() => hideDetail()} to='/cards'>Return to Cards List</Link>
+          <div className='flex-col card-detail-all'>
+            <Link className='ml-sm-4 general-link' onClick={() => hideDetail()} to='/cards'>Return to Cards List</Link>
 
-            <Accordion style={{ width: '700px' }} className='flex' defaultActiveKey='0'>
+            <Accordion className='card-detail-holder flex' defaultActiveKey='0'>
               <Card>
                 <Card.Body>
                   <Accordion.Toggle as={Card.Body} eventKey='1'>
@@ -47,25 +47,25 @@ const CardDetail = ({ idx, token, pk, setViewDetail, viewDetail, cards }) => {
               </Card>
               <Card>
                 <Accordion.Collapse className='animate__animated animate__fadeInLeft' eventKey='1'>
-                  <Card.Body><div style={{ border: `20px solid ${card.border}`, paddingLeft: '70px', paddingRight: '70px', justifyContent: 'center', alignItems: 'center', backgroundColor: `${card.background}` }} className='flex mybackground '><span style={{ fontFamily: `${card.font}` }} className='myfont-small'>{card.message}</span></div></Card.Body>
+                  <Card.Body><div className='inside-body flex' style={{ border: `20px solid ${card.border}`, justifyContent: 'center', alignItems: 'center', backgroundColor: `${card.background}` }}><span style={{ fontFamily: `${card.font}` }} className='myfont-small'>{card.message}</span></div></Card.Body>
                 </Accordion.Collapse>
               </Card>
             </Accordion>
-            <Navbar bg='dark' variant='dark' style={{ backgroundImage: 'none', justifyContent: 'space-between', width: '700px' }}>
+            <Navbar className='card-detail-navbar'>
               <Navbar.Text style={{ color: 'white' }}>
-                <span style={{ fontSize: '25px' }} className='material-icons'>favorite_border</span>
+                <span className='material-icons sm-nav-icon'>favorite_border</span>
               </Navbar.Text>
               <Navbar.Text style={{ color: 'white' }}>
-                <span style={{ fontSize: '25px' }} className='material-icons'>edit</span>
+                <span className='material-icons sm-nav-icon'>edit</span>
               </Navbar.Text>
               <Navbar.Text style={{ color: 'white' }}>
-                <span style={{ fontSize: '25px' }} className='material-icons'>delete</span>
+                <span className='material-icons sm-nav-icon'>delete</span>
               </Navbar.Text>
               <Navbar.Text onClick={() => getPreviousCard(idx)} style={{ color: 'white' }}>
-                <span style={{ fontSize: '25px' }} className='material-icons'>arrow_back</span>
+                <span className='material-icons sm-nav-icon'>arrow_back</span>
               </Navbar.Text>
               <Navbar.Text onClick={() => getNextCard(idx)} style={{ color: 'white' }}>
-                <span style={{ fontSize: '25px' }} className='material-icons'>arrow_forward</span>
+                <span className='material-icons sm-nav-icon'>arrow_forward</span>
               </Navbar.Text>
             </Navbar>
           </div>
