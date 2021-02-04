630155a4d5b579d9bd02aeb24446d7643a206379
Tracy Falba <tfalba@mac.com>
2021-02-04 10:34:34 -0500

    split view of list and detail

--- frontend/src/components/CardDetail.js
+++ frontend/src/components/CardDetail.js
@@ -1,12 +1,13 @@
 import { getCardDetail } from '../api'
+// import 'font-awesome/css/font-awesome.min.css'
 import { useState, useEffect } from 'react'
 import { Redirect, Link } from 'react-router-dom'
-import Accordion from 'react-bootstrap/Accordion'
-import Card from 'react-bootstrap/Card'
+// import Accordion from 'react-bootstrap/Accordion'
+// import Card from 'react-bootstrap/Card'
 import Carousel from 'react-bootstrap/Carousel'
 import Navbar from 'react-bootstrap/Navbar'
 
-const CardDetail = ({ token, pk }) => {
+const CardDetail = ({ token, pk, setViewDetail, viewDetail, cards }) => {
   const [card, setCard] = useState([])
   useEffect(() => {
     getCardDetail(token, pk).then(card => setCard(card))
@@ -14,6 +15,18 @@ const CardDetail = ({ token, pk }) => {
 
   if (!token) {
     return <Redirect to='/login' />
+  }
+  function getNextCard (id) {
+    setCard(cards[id])
+  }
+  function getPreviousCard (id) {
+    setCard(cards[id - 1])
+  }
+
+  function hideDetail () {
+    if (viewDetail) {
+      setViewDetail(false)
+    }
   }
   return (
     <>
@@ -48,25 +61,33 @@ const CardDetail = ({ token, pk }) => {
               </Navbar.Text>
             </Navbar>
           </div> */}
-          <div className='flex-col' style={{ alignItems: 'center', marginTop: '50px' }}>
+
+          <div className='flex-col' style={{ alignItems: 'center', marginTop: '40px' }}>
+            <Link className='ml-sm-4' style={{ fontSize: '20px' }} onClick={() => hideDetail()} to='/cards'>Return to Cards List</Link>
             <Carousel>
               <Carousel.Item>
-                <div style={{ border: `10px solid ${card.background}`, color: 'white', backgroundImage: `url(${card.image_front}` }} className='myimage myfont-big'>{card.title}</div>
+                <div style={{ border: `20px solid ${card.background}`, color: 'white', backgroundImage: `url(${card.image_front}` }} className='myimage myfont-big'>{card.title}</div>
               </Carousel.Item>
               <Carousel.Item>
-                <div style={{ border: `10px solid ${card.background}`, paddingLeft: '70px', paddingRight: '70px', justifyContent: 'flex-start', alignItems: 'center', backgroundColor: '#12125f' }} className='flex mybackground '><span className='myfont-small'>{card.message}</span></div>
+                <div style={{ border: `20px solid ${card.background}`, paddingLeft: '70px', paddingRight: '70px', justifyContent: 'flex-start', alignItems: 'center', backgroundColor: '#12125f' }} className='flex mybackground '><span style={{ fontFamily: `${card.font}` }} className='myfont-small'>{card.message}</span></div>
               </Carousel.Item>
             </Carousel>
             <Navbar style={{ backgroundImage: 'none', justifyContent: 'space-between', width: '750px', backgroundColor: 'black', color: 'white' }}>
-              <Navbar.Brand style={{ color: 'white' }}><Link to='/cards'>Return to Cards List</Link></Navbar.Brand>
-              <Navbar.Text style={{ color: 'white' }}>
+
+              <Navbar.Text style={{ color: 'white', fontSize: '20px' }}>
                 Favorite
               </Navbar.Text>
-              <Navbar.Text style={{ color: 'white' }}>
+              <Navbar.Text style={{ color: 'white', fontSize: '20px' }}>
                 Edit card
               </Navbar.Text>
-              <Navbar.Text style={{ color: 'white' }}>
+              <Navbar.Text style={{ color: 'white', fontSize: '20px' }}>
                 Delete card
+              </Navbar.Text>
+              <Navbar.Text onClick={() => getPreviousCard(pk)} style={{ color: 'white', fontSize: '20px' }}>
+                Previous card
+              </Navbar.Text>
+              <Navbar.Text onClick={() => getNextCard(pk)} style={{ color: 'white', fontSize: '20px' }}>
+                Next card
               </Navbar.Text>
             </Navbar>
           </div>
